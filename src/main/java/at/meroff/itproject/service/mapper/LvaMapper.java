993269521985df484c55a633e2036e08c8a2601e
tfragner <fragner@bitter.at>
2017-07-15 22:08:51 +0200

    LVA DTO beinhaltet jetzt Termine

--- src/main/java/at/meroff/itproject/service/mapper/LvaMapper.java
+++ src/main/java/at/meroff/itproject/service/mapper/LvaMapper.java
@@ -8,14 +8,15 @@ import org.mapstruct.*;
 /**
  * Mapper for the entity Lva and its DTO LvaDTO.
  */
-@Mapper(componentModel = "spring", uses = {SubjectMapper.class, InstituteMapper.class, })
+@Mapper(componentModel = "spring", uses = {SubjectMapper.class, InstituteMapper.class, AppointmentMapper.class})
 public interface LvaMapper extends EntityMapper <LvaDTO, Lva> {
 
     @Mapping(source = "subject.id", target = "subjectId")
     @Mapping(source = "subject.subjectName", target = "subjectSubjectName")
 
     @Mapping(source = "institute.id", target = "instituteId")
-    LvaDTO toDto(Lva lva); 
+    @Mapping(target = "appointments")
+    LvaDTO toDto(Lva lva);
     @Mapping(target = "appointments", ignore = true)
     @Mapping(target = "csl1S", ignore = true)
     @Mapping(target = "csl2S", ignore = true)
@@ -24,7 +25,7 @@ public interface LvaMapper extends EntityMapper <LvaDTO, Lva> {
     @Mapping(target = "curriculumsubjects", ignore = true)
 
     @Mapping(source = "instituteId", target = "institute")
-    Lva toEntity(LvaDTO lvaDTO); 
+    Lva toEntity(LvaDTO lvaDTO);
     default Lva fromId(Long id) {
         if (id == null) {
             return null;
