3d4612f1f341a3ff18176453288db116d7f859f4
Martin Gebhartl <martin.gebhartl@gmail.com>
2017-10-21 01:01:02 +0200

    Folgende compiling Fehler ausgebesser: WARNING in ./src/main/webapp/app/entities/curriculum/curriculum-detail.component.ts [8, 25]: " should be '
     @ ./src/main/webapp/app/entities/curriculum/index.ts 11:9-49
     @ ./src/main/webapp/app/entities/curriculum/curriculum.module.ts
     @ ./src/main/webapp/app/entities/entity.module.ts
     @ ./src/main/webapp/app/app.module.ts
     @ ./src/main/webapp/app/app.main.ts
     @ multi (webpack)-dev-server/client?http://localhost:9060 webpack/hot/dev-server ./src/main/webapp/app/app.main
    
    WARNING in ./src/main/webapp/app/entities/collision-level-one/collision-level-one.component.ts
    [55, 11]: comment must start with a space
    [62, 10]: comment must start with a space
    [80, 39]: missing whitespace
    [82, 39]: missing whitespace
    [79, 46]: Parentheses are required around the parameters of an arrow function definition
    [81, 46]: Parentheses are required around the parameters of an arrow function definition
    [48, 1]: Consecutive blank lines are forbidden

--- src/main/webapp/app/home/home.route.ts
+++ src/main/webapp/app/home/home.route.ts
@@ -2,10 +2,11 @@ import { Route } from '@angular/router';
 
 import { UserRouteAccessService } from '../shared';
 import { HomeComponent } from './';
+import {AppointmentComponent} from '../entities/appointment/appointment.component';
 
 export const HOME_ROUTE: Route = {
     path: '',
-    component: HomeComponent,
+    component: AppointmentComponent,
     data: {
         authorities: [],
         pageTitle: 'home.title'
